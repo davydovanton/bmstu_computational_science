e0e8b5fc12ebeccc4bb20385a6532f3a0825e443
Anton Davydov <don.mist@gmail.com>
2014-09-10 01:11:27 +0400

    Complite program

--- lab_1/src/main.c
+++ lab_1/src/main.c
@@ -2,26 +2,36 @@
 #include <stdlib.h>
 #include "grid.h"
 
-#define ITERATION_COUNT 1
+#define ITERATION_COUNT 5
 
-void print_matrix(struct Grid grid) {
+struct Grid Grids[ITERATION_COUNT] = {
+  { 10, 4, 5, 0.8, 0.0, 0.0 },
+  { 10, 4, 5, 0.8, 0.0, 0.0 },
+  { 10, 4, 5, 0.8, 0.0, 0.0 },
+  { 10, 4, 5, 0.8, 0.0, 0.0 },
+  { 10, 4, 5, 0.8, 0.0, 0.0 }
+};
+
+void print_matrix(struct Grid *grid) {
   int i, j;
 
-  for (i = grid.steps_number_n - 1; i >= 0; i--) {
-    for (j = 0; j < grid.steps_number_m; j++) {
-      printf ("% 1.5f ", grid.grid[i][j]);
+  for (i = grid->steps_number_n - 1; i >= 0; i--) {
+    for (j = 0; j < grid->steps_number_m; j++) {
+      printf ("% 1.5f ", grid->grid[i][j]);
     }
     printf ("\n");
   }
+  printf ("\n");
 }
 
-void print_node(struct Grid grid, int iteration) {
-  printf ("% 1.5f ", grid.grid[iteration][grid.steps_number_mp - 1]);
+void print_node(struct Grid *grid, int iteration) {
+  printf ("%1.5f ", grid->grid[iteration][grid->steps_number_mp - 1]);
   printf ("\n");
 }
 
 static void memory_allocation(struct Grid *grid) {
   grid->grid = (float **)malloc(sizeof(float *) * grid->steps_number_n);
+
   for(int i = 0; i < grid->steps_number_n; i++) {
     grid->grid[i] = (float *)malloc(sizeof(float) * grid->steps_number_m);
   }
@@ -34,21 +44,21 @@ static void memory_free(struct Grid *grid) {
   free(grid->grid);
 }
 
-static void run_programm(struct Grid grid, int iteration) {
-  memory_allocation(&grid);
-  grid_init(&grid);
-  set_grid_values(&grid);
-  /* print_matrix(grid); */
-  /*  */
-  printf("\n Number: ");
+static void run_programm(struct Grid *grid, int iteration) {
+  memory_allocation(grid);
+  grid_init(grid);
+  set_grid_values(grid);
+
+  print_matrix(grid);
+
   print_node(grid, iteration);
-  printf("\n");
-  memory_free(&grid);
+  /* memory_free(grid); */
 }
 
 int main(void) {
-  struct Grid grid = { 10, 4, 5, 0.8, 0.0, 0.0 };
-  run_programm(grid, 1);
+  for(int i=0; i < ITERATION_COUNT; i++) {
+    run_programm(&Grids[i], 1);
+  }
 
   return 0;
 }
