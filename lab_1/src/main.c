56c23c50c03d6fbca92b248298090e9f39a0fe47
Anton Davydov <don.mist@gmail.com>
2014-09-23 20:24:58 +0400

    Update first lab

--- lab_1/src/main.c
+++ lab_1/src/main.c
@@ -5,11 +5,11 @@
 #define ITERATION_COUNT 5
 
 struct Portion Portions[ITERATION_COUNT] = {
-  { 101,  41, 10, 0.8, 0.0, 0.0 },
-  { 201,  81, 20, 0.8, 0.0, 0.0 },
-  { 401, 161, 40, 0.8, 0.0, 0.0 },
-  { 101,  81, 10, 0.4, 0.0, 0.0 },
-  { 101,  21, 10, 1.6, 0.0, 0.0 }
+  { 101,  41, 10, 0.8,   0.01, 0.0 },
+  { 201,  81, 20, 0.8,  0.005, 0.0 },
+  { 401, 161, 40, 0.8, 0.0025, 0.0 },
+  { 101,  81, 10, 0.4,   0.01, 0.0 },
+  { 101,  21, 10, 1.6,   0.01, 0.0 }
 };
 
 void print_matrix(struct Portion *portion) {
@@ -24,38 +24,52 @@ void print_matrix(struct Portion *portion) {
   printf ("\n");
 }
 
-void print_node(struct Portion *portion, int iteration) {
-  printf ("%1.5f\n", portion->grid[iteration][portion->steps_number_mp - 1]);
+void print_node(struct Portion *portion) {
+  int j;
+
+  for (j = 0; j < portion->steps_number_m; j = j + portion->steps_number_mp) {
+    printf ("X value: %  d; U(x, t) value: % 1.5f; U(x, t) analytic value: % 1.5f",
+        j,
+        portion->grid[portion->steps_number_n - 1][j],
+        analytic_function(j, portion->steps_number_n - 1));
+    printf ("\n");
+  }
+  printf ("\n\n\n");
 }
 
 static void memory_allocation(struct Portion *portion) {
-  portion->grid = (float **)malloc(sizeof(float *) * portion->steps_number_n);
+  int i;
 
-  for(int i = 0; i < portion->steps_number_n; i++) {
-    portion->grid[i] = (float *)malloc(sizeof(float) * portion->steps_number_m);
+  portion->grid = (double **)malloc(sizeof(double *) * portion->steps_number_n);
+
+  for(i = 0; i < portion->steps_number_n; i++) {
+    portion->grid[i] = (double *)malloc(sizeof(double) * portion->steps_number_m);
   }
 }
 
 static void memory_free(struct Portion *portion) {
-  for(int i=0; i < portion->steps_number_n; i++) {
+  int i;
+
+  for(i=0; i < portion->steps_number_n; i++) {
     free(portion->grid[i]);
   }
   free(portion->grid);
 }
 
-static void run_programm(struct Portion *portion, int iteration) {
+static void run_programm(struct Portion *portion) {
   memory_allocation(portion);
   portion_init(portion);
   set_portion_values(portion);
 
-  print_node(portion, iteration);
+  print_node(portion);
   memory_free(portion);
 }
 
 int main(void) {
-  for(int i = 0; i < ITERATION_COUNT; i++) {
-    run_programm(&Portions[i], i + 1);
-  }
+  int i;
 
+  for(i = 0; i < ITERATION_COUNT; i++) {
+    run_programm(&Portions[i]);
+  }
   return 0;
 }
--- lab_1/src/portion.h
+++ lab_1/src/portion.h
@@ -3,45 +3,47 @@
 #include "math_functions.h"
 
 #define COEFFICIENT_A 1
-#define MAX_X_VALUE 1.0
 
 struct Portion {
   int steps_number_m;
   int steps_number_n;
   int steps_number_mp;
-  float kurant_number;
-  float h_index;
-  float tau_index;
-  float **grid;
+  double kurant_number;
+  double h_index;
+  double tau_index;
+  double **grid;
 };
 
-static float top_portion_value(struct Portion *portion, int n, int j) {
-  float center_y = portion->grid[n][j];
-  float kurant_number = portion->kurant_number;
-  float left_y = portion->grid[n][j - 1];
-  float tau = portion->tau_index;
-  float f_function_data = f_function(n * portion->tau_index);
+static double top_portion_value(struct Portion *portion, int n, int j) {
+  double center_y = portion->grid[n][j];
+  double kurant_number = portion->kurant_number;
+  double left_y = portion->grid[n][j - 1];
+  double tau = portion->tau_index;
+  double f_function_data = f_function(n * portion->tau_index);
 
   return center_y - kurant_number * (center_y - left_y) + tau * f_function_data;
 }
 
 static void set_portion_values(struct Portion *portion) {
-  for (int i = 1; i < portion->steps_number_n; i++) {
-    for (int j = 1; j < portion->steps_number_m; j++) {
+  int i, j;
+
+  for (i = 1; i < portion->steps_number_n; i++) {
+    for (j = 1; j < portion->steps_number_m; j++) {
       portion->grid[i][j] = top_portion_value(portion, i - 1, j);
     }
   }
 }
 
 static void portion_init(struct Portion *portion) {
-  portion->h_index = MAX_X_VALUE / portion->steps_number_m;
+  int i, j;
+
   portion->tau_index = ((portion->h_index * portion->kurant_number) / COEFFICIENT_A);
 
-  for (int i = 0; i < portion->steps_number_m; i++) {
+  for (i = 0; i < portion->steps_number_m; i++) {
     portion->grid[0][i] = phi_function(i * portion->h_index);
   }
 
-  for (int j = 0; j < portion->steps_number_n; j++) {
+  for (j = 0; j < portion->steps_number_n; j++) {
     portion->grid[j][0] = alpha_function(j * portion->tau_index);
   }
 }
